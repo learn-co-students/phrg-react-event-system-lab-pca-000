2f06d97e15e57a4baf5ded66aafe16122a089c05
KFad <kfadigan@yahoo.com>
2019-07-22 09:13:37 -0400

    Using events to do things

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,31 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  constructor() {
+    super();
+
+    this.blurring = this.blurring.bind(this);
+
+    this.focus = this.focus.bind(this);
+  }
+
+
+  blurring() {
+    console.log('Hey! Eyes on me!');
+  }
+
+
+  focus() {
+    console.log(`Good!`);
+  }
+
+
+  render() {
+    return (
+        <button onFocus={this.focus} onBlur={this.blurring}>Button</button>
+    );
+  }
+
+};
